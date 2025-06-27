733a2e7bbd5731837b8eff589ca8cea5975c9cb8
wishtoday238 <wishtoday238@gmail.com>
2025-06-27 19:03:42 +0800

    test

--- src/main/java/aorg/example/worldBaeter/WorldBaeter.java
+++ src/main/java/aorg/example/worldBaeter/WorldBaeter.java
@@ -1,59 +1,41 @@
 package aorg.example.worldBaeter;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
-public final class WorldBaeter extends JavaPlugin {
+public class WorldBaeter extends JavaPlugin {
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-package aorg.example.worldBaeter;
+        getLogger().info("交易市场插件已启用!");
+        // 注册命令
+        getCommand("market").setExecutor(this);
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-        public final class WorldBaeter extends JavaPlugin {
-
-            @Override
-            public void onEnable() {
-                getLogger().info("交易市场插件已启用!");
-                // 注册命令
-                getCommand("market").setExecutor(this);
-
-                // 注册事件监听器
-                getServer().getPluginManager().registerEvents(new MarketListener(), this);
-            }
-
-            @Override
-            public void onDisable() {
-                getLogger().info("交易市场插件已禁用");
-            }
-
-            @Override
-            public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-                if (cmd.getName().equalsIgnoreCase("market")) {
-                    if (!(sender instanceof Player)) {
-                        sender.sendMessage("§c只有玩家可以使用此命令!");
-                        return true;
-                    }
-
-                    Player player = (Player) sender;
-                    // 打开交易市场界面
-                    player.openInventory(MarketGUI.createMarketGUI());
-                    return true;
-                }
-                return false;
-            }
-        }
-
-
-        //wishtoday238's code
+        // 注册事件监听器
+        getServer().getPluginManager().registerEvents(new MarketListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("交易市场插件已禁用");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("market")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("§c只有玩家可以使用此命令!");
+                return true;
+            }
+
+            Player player = (Player) sender;
+            // 打开交易市场界面
+            player.openInventory(MarketGUI.createMarketGUI());
+            return true;
+        }
+        return false;
     }
 }
